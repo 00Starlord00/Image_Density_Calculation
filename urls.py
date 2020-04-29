e3764da5f53a960759e6d78b0a3754016db84231
00Starlord00 <41700392+00Starlord00@users.noreply.github.com>
2020-04-29 17:06:32 +0530

    Added Django files
    These files are to be replaced with settings.py and urls.py in the main project directory

--- urls.py
+++ urls.py
@@ -1,11 +1,7 @@
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('dataProcess', views.dataProcess, name='dataProcess'),
+    path('admin/', admin.site.urls),
+    path('', include('mainFiles.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
